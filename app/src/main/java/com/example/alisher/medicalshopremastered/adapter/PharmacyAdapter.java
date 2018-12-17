63ea10f2a750ef1efa3d5d0d8b38da1abcbd0eb1
AlisherKassymov <37921839+AlisherKassymov@users.noreply.github.com>
2018-12-17 23:02:35 +0600

    Added Pharmacy implementation

---
+++ app/src/main/java/com/example/alisher/medicalshopremastered/adapter/PharmacyAdapter.java
@@ -0,0 +1,95 @@
+package com.example.alisher.medicalshopremastered.adapter;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+import com.example.alisher.medicalshopremastered.R;
+import com.example.alisher.medicalshopremastered.enitity.Pharmacy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PharmacyAdapter extends RecyclerView.Adapter<PharmacyAdapter.MyViewHolder> implements Filterable {
+    private List<Pharmacy> itemsData;
+    private List<Pharmacy> itemsFilteredData;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public CardView cardView;
+        public TextView textView1,textView2;
+        public View mView;
+
+        public MyViewHolder(View view){
+            super(view);
+            mView=view;
+            cardView=(CardView) view.findViewById(R.id.pharmacy_card_view);
+            textView1=(TextView) view.findViewById(R.id.pharmacyName);
+            textView2=(TextView) view.findViewById(R.id.pharmacyTime);
+        }
+    }
+
+    public PharmacyAdapter(List<Pharmacy> itemsData){
+        this.itemsData=itemsData;
+        this.itemsFilteredData=itemsData;
+    }
+
+
+    @Override
+    public PharmacyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,int viewType){
+        View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.pharmacy_card,parent,false);
+        final MyViewHolder viewHolder=new MyViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position){
+        holder.textView1.setText(itemsData.get(position).getPharmName());
+        holder.textView2.setText(itemsData.get(position).getTime_at());
+    }
+
+    @Override
+    public int getItemCount(){
+        return itemsFilteredData.size();
+    }
+
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    itemsFilteredData = itemsData;
+                } else {
+                    List<Pharmacy> filteredList = new ArrayList<>();
+                    for (Pharmacy row : itemsData) {
+
+                        // name match condition. this might differ depending on your requirement
+                        // here we are looking for name or phone number match
+                        if (row.getPharmName().toLowerCase().contains(charString.toLowerCase()) || row.getTime_at().contains(charSequence)) {
+                            filteredList.add(row);
+                        }
+                    }
+
+                    itemsFilteredData = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = itemsFilteredData;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                itemsFilteredData = (ArrayList<Pharmacy>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+}
